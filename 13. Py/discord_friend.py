58e13a88256ae2f57d1cea3909b2fd9cb1b5930e
Sasha <lonagi22@gmail.com>
2020-12-15 17:20:54 +0200

    Update discord_friend.py

--- 13. Py/discord_friend.py
+++ 13. Py/discord_friend.py
@@ -1,37 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[19]:
-
-
 from pynput.mouse import Button, Controller
 from time import sleep
 import keyboard
 
-
-# In[20]:
-
-
 mouse = Controller()
-
-
-# In[54]:
-
-
 # Read pointer position
 print('The current pointer position is {0}'.format(
     mouse.position))
 
-
-# In[55]:
-
-
 Search_pos = (2430, 248)
 BTN_pos = (3250, 233)
-
-
-# In[48]:
-
 
 def click_to_input():
     mouse.position = Search_pos
@@ -43,10 +23,6 @@ def click_to_add():
     mouse.press(Button.left)
     mouse.release(Button.left)
 
-
-# In[49]:
-
-
 #Clear input
 def clear_input():
     for _ in range(10):
@@ -54,18 +30,10 @@ def clear_input():
         keyboard.press_and_release("backspace")
         sleep(0.1)
 
-
-# In[57]:
-
-
 def do_input(arr):
     for i in arr:
         keyboard.write(i)
         sleep(0.1)
-
-
-# In[58]:
-
 
 for i in range(200,1000):
     clear_input()
@@ -78,10 +46,3 @@ for i in range(200,1000):
     sleep(1.5)
     keyboard.press_and_release("enter")
     sleep(7)
-
-
-# In[ ]:
-
-
-
-
