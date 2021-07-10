82b48029c822f068363d184444c8bc256cc563e1
Sasha <lonagi22@gmail.com>
2021-07-10 15:44:32 +0300

    Update img2pdf.py

--- 11. Tools/img2pdf.py
+++ 11. Tools/img2pdf.py
@@ -7,6 +7,6 @@ im_list = []
 
 for file in os.listdir(path=path):
     if file.split(".")[-1] in ("jpg","png","svg","jpeg","gif"):
-        im_list.append(Image.open(path+file))
+        im_list.append(Image.open(path+file).convert("RGB"))
 
 im_list[0].save(pdf_filename, "PDF" ,resolution=100.0, save_all=True, append_images=im_list[1:])
